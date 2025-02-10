418b3046c89890a3919b72a3522ce02399c66af3
Finn Busse <finnbusse@outlook.de>
2025-02-10 08:11:29 +0100

    add stuff

--- Muehlenfluegel.java
+++ Muehlenfluegel.java
@@ -2,7 +2,7 @@ import sas.*;
 import java.awt.Color; 
 public class Muehlenfluegel {
   Rectangle fluegel;
-  
+  Rectangle raster;
   public Muehlenfluegel(double x,double y) {
       fluegel = new Rectangle(x,y+20,5,80);
       fluegel = new Rectangle(x+25,y+20,5,80);
