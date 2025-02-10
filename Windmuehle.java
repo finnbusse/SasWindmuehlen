0d2e547ca3d87ef31a51533ce7d74d6016d36f0a
Finn Busse <finnbusse@outlook.de>
2025-02-10 08:47:48 +0100

    getter setter conflict

--- Windmuehle.java
+++ Windmuehle.java
@@ -14,7 +14,7 @@ public class Windmuehle {
   
   // end attributes
   public Windmuehle(double px,double py){
-      muehlenhaus = new Muehlenhaus(px,py);
+      muehlenhaus = new Muehlenhaus(px+300,py-1000);
       muehlenrad = new Muehlenrad(px+50,py-10);
   }
   // start methods
