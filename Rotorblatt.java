3962bc4a7be94ad5218adf1f946ee31b6f1e925d
Finn Busse <finnbusse@outlook.de>
2025-02-10 08:52:27 +0100

    getter setter conflict

--- Rotorblatt.java
+++ Rotorblatt.java
@@ -14,7 +14,7 @@ public class Rotorblatt {
     private Ellipse blatt;
     double pA,pB,pC;
     public Rotorblatt(double pX,double pY){
-        blatt = new Ellipse(pX,pY,100,30,new Color(202,202,202));
+        blatt = new Ellipse(pX,pY,100,30,new Color(190,190,190));
     }
   // start methods
     public void drehen(double pA,double pB,double pC)
