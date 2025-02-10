71d987fba721ef1d985e066edf5b6dafc86f70e8
Finn Busse <finnbusse@outlook.de>
2025-02-10 09:06:36 +0100

    nearly there

--- Rotor.java
+++ Rotor.java
@@ -25,7 +25,7 @@ public class Rotor {
         rotorblatt2 = new Rotorblatt(pX,pY);
         rotorblatt3 = new Rotorblatt(pX,pY);
         rotorblatt1.drehen(0);
-        rotorblatt2.drehen(pX, pY, 120);
+        rotorblatt2.drehen(120);
         rotorblatt3.drehen(pX, pY, 240);
         x = pX;
         y = pY;
