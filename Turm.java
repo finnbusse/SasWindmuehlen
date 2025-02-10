22be63751f2244ae56332b3d21e36b0a204a69ae
Finn Busse <finnbusse@outlook.de>
2025-02-10 08:19:38 +0100

    testing

--- Turm.java
+++ Turm.java
@@ -14,7 +14,7 @@ public class Turm {
         Rectangle turm = new Rectangle(px,py,12,180, new Color(232,232,232));
         Circle turbine = new Circle(px-7,py-7,14,new Color(232,232,232));
         Rectangle gruen1 = new Rectangle(px,py+150,12,30,new Color(30,230,40));
-        Rectangle gruen2 = new Rectangle(px,py+160,12,20
+        Rectangle gruen2 = new Rectangle(px,py+160,12,20,new Color(30,200,40));
         Rectangle gruen3 = new Rectangle(px,py+170,12,10,new Color(30,160,40));
         Rectangle tuer = new Rectangle(px+4,py+168,4,12,new Color(200,200,200));
         Circle tuergriff = new Circle(px+4,py+174,1,new Color(232,232,232));
