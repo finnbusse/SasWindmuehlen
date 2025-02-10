c9450acacd1e7a89047187f451c07e6eb031d13e
Finn Busse <finnbusse@outlook.de>
2025-02-10 08:21:03 +0100

    testing

--- Turm.java
+++ Turm.java
@@ -13,7 +13,7 @@ public class Turm {
         View fenster = new View(700,800);
         Rectangle turm = new Rectangle(px,py,12,180, new Color(232,232,232));
         Circle turbine = new Circle(px-7,py-7,14,new Color(232,232,232));
-        
+        Rectangle gruen0 = new Rectangle(px,py+140,12,40,new Color(100,230,100));
         Rectangle gruen1 = new Rectangle(px,py+150,12,30,new Color(30,230,40));
         Rectangle gruen2 = new Rectangle(px,py+160,12,20,new Color(30,200,40));
         Rectangle gruen3 = new Rectangle(px,py+170,12,10,new Color(30,160,40));
