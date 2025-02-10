94b9ad6bbb5208ab2197be687c85bfd3cca329a7
Finn Busse <finnbusse@outlook.de>
2025-02-10 08:21:40 +0100

    testing

--- Muehlenfluegel.java
+++ Muehlenfluegel.java
@@ -4,7 +4,7 @@ public class Muehlenfluegel {
   Rectangle fluegel1;
   Rectangle fluegel2;
   Rectangle raster[];
-  private Muehlenfluegel(double x,double y) {
+  public Muehlenfluegel(double x,double y) {
       fluegel1 = new Rectangle(x,y+20,5,80);
       //fluegel = new Rectangle(x+25,y+20,5,80);
       //for (int i=0; i < 9; i++) {
