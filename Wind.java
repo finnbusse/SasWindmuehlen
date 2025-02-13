3854fdc2cf73832915dbf48ddbc8b8d8e613cf6c
Finn Busse <finnbusse@outlook.de>
2025-02-13 12:55:23 +0100

    finalizing threads

--- Wind.java
+++ Wind.java
@@ -1,19 +1,23 @@
 import sas.*;
 
 public class Wind {
-
     private int windstaerke;
-    public Wind(){
+
+    public Wind() {
         windstaerke = 2;
     }
-    
-    public void setWindstaerke(int pWindstaerke){
-        windstaerke = pWindstaerke;
-        System.out.println("Die Windst�rke wurde auf " + windstaerke);
+
+    public void setWindstaerke(int pWindstaerke) {
+        if (pWindstaerke <= 12 && pWindstaerke >= 0) {
+            System.out.println("Die Windstaerke wurde von " + windstaerke + " auf " + pWindstaerke);
+            windstaerke = pWindstaerke;
+        } else {
+            System.err.println("Die Windstaerke " + pWindstaerke + " ist ungültig!");
+            throw new RuntimeException("ungültige Windstaerke!");
+        }
     }
 
-    public int getWindstaerke(){
+    public int getWindstaerke() {
         return windstaerke;
     }
-
 }
